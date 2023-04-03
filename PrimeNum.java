4c1e36da83c5bf76f9bb0bea530af016f8681848
SunilKumarPradhan <sunilkumarweb47@gmail.com>
2023-04-03 21:14:28 +0530

    initial commit

---
+++ PrimeNum.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class PrimeNum{
+public static void main(String[] args) {
+
+    int n, counter=0; 
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter a number: ");
+    n = sc.nextInt();
+
+    for (int i = 1; i <= n; i++) {
+        if (n % i == 0) {
+            counter++;
+        }
+    }
+
+    if (counter==2)
+      System.out.println(n + " is a prime number.");
+    else
+      System.out.println(n + " is not a prime number."); 
+  }
+}
